2c528f886302acff7a811f8508f7471eea5bc665
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2022-11-09 17:26:16 -0300

    Refatorando o código e criando a interface da foto

--- src/pages/fotos/Fotos.js
+++ src/pages/fotos/Fotos.js
@@ -1,7 +1,6 @@
 import "./Fotos.css";
 import Hero from "../../componentes/hero/Hero";
 import ContentDataApiQuery from "../../componentes/lastedContent/ContentDataApiQuery";
-import useGetData from "../../hooks/getData/useGetData";
 
 export default function Fotos() {
   return (
--- src/pages/fotos/SingleFoto.js
+++ src/pages/fotos/SingleFoto.js
@@ -1,6 +1,48 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import Loading from "../../assets/loading.svg";
+
+import "./SingleFotos.css";
 
 export default function SingleFoto() {
+  const { id } = useParams();
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.pexels.com/v1/photos/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: process.env.REACT_APP_API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setUser(response);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
   return (
-    <div>SingleFoto</div>
-  )
+    <>
+      <div className="container">
+        {user.length === 0 ? (
+          <div className="loading">
+            <img src={Loading} alt="Loading" />
+          </div>
+        ) : (
+          <>
+            <div className="imgContent">
+              <img src={user?.src?.original} alt={user?.photographer} />
+            </div>
+            <div className="infoContent">
+              <p>id: {user.id}</p>
+              <h3>Photographer: {user.photographer}</h3>
+              <a href={user.photographer_url}>Ver mais</a>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
